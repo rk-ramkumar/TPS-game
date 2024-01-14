258b3c9bc731d102976e86152990d86d8ccb15a4
ram <rramkumar1306@example.com>
2024-01-14 19:56:21 +0530

    Dusted.

--- scripts/Variables.gd
+++ scripts/Variables.gd
@@ -5,11 +5,3 @@ var current_character = "eren"
 var characterList = ["eren", "jaw"]
 var currentMap
 var kills = 0
-
-func _notification(what):
-	var backRequest = what == NOTIFICATION_WM_WINDOW_FOCUS_OUT or what == NOTIFICATION_WM_GO_BACK_REQUEST
-
-	if backRequest:
-		get_tree().set_pause(true)
-		get_tree().change_scene_to_file("res://scenes/pause_screen.tscn")
-		pass
