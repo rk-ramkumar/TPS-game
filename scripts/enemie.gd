8096ee0146089cd072b4615e7ba28c853273704a
ram <rramkumar1306@example.com>
2024-01-16 13:07:48 +0530

    Refactored.

--- scripts/enemie.gd
+++ scripts/enemie.gd
@@ -4,6 +4,15 @@ extends CharacterBody3D
 @onready var nav_agent = $NavigationAgent3D
 
 const ATTACK_RANGE = 1.0
+enum states {
+	IDLE,
+	ATTACK,
+	ROAR,
+	DEAD,
+	WALK,
+	RUN,
+	CRAWL
+}
 
 var player = null
 var speed = 4.0
@@ -20,16 +29,21 @@ func _process(delta):
 	
 	# Added Gravity.
 	if not is_on_floor():
+		print("s")
 		velocity.y -= gravity * delta
-	
+
 
 	nav_agent.set_target_position(player.global_position)
 	var next_nav_point = nav_agent.get_next_path_position()
 	velocity = (next_nav_point - global_position).normalized() * speed
-	look_at(Vector3(player.global_position.x + velocity.x, global_position.y, player.global_position.z + velocity.z), Vector3.UP)
+	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
 
 	move_and_slide()
 	
 
 func _target_in_range():
 	return global_position.distance_to(player.global_position) < ATTACK_RANGE
+
+
+func _on_area_3d_area_entered(area):
+	print(area)
