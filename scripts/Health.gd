bd5c6386bbb48d62597aa5302bd07c72d38c33f6
ram <rramkumar1306@example.com>
2024-01-16 11:43:55 +0530

    Introduced Health component.

---
+++ scripts/Health.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var max_health : int = 100
+var current_health : int = max_health
+
+signal died
+
+func take_damage(amount):
+	# Reduce health when taking damage
+	current_health -= amount
+
+	# Check if health is zero or below
+	if current_health <= 0:
+		emit_signal("died")
